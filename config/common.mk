c8845c9a111da8fa680c5046267e4295f9990a76
Mathieu Chartier <mathieuc@google.com>
2025-10-14 19:58:39 +0800

    config: Reduce system server verbosity
    Test: lunch statix_cannon-user && m
    Change-Id: I58d2e993ae9a7213e94661d22b47c28b0a2243e8
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/13531

--- config/common.mk
+++ config/common.mk
@@ -128,6 +128,9 @@ SYSTEMUI_OPTIMIZE_JAVA ?= true
 # Disable vendor restrictions
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+# Reduce system server verbosity
+PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
+
 ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
 $(call enforce-product-packages-exist-internal,$(lastword $(_include_stack)),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
