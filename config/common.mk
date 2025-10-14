de9e34c37b288d0f910545afb32588afc51ac514
Yurical <yurical1@outlook.com>
2025-10-14 20:00:09 +0800

    config: Do not include other java mini debug info
    In the same manner of PRODUCT_SYSTEM_SERVER_DEBUG_INFO.
    We explicitly add PRODUCT_OTHER_JAVA_DEBUG_INFO here since
    WITH_DEXPREOPT_DEBUG_INFO can be overriden with
    PRODUCT_SYSTEM_SERVER_DEBUG_INFO and PRODUCT_OTHER_JAVA_DEBUG_INFO.
    
    Change-Id: Ib9173bdbeaf3e279256be10c4082036c486dd154
    Signed-off-by: Yurical <yurical1@outlook.com>

--- config/common.mk
+++ config/common.mk
@@ -131,6 +131,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Reduce system server verbosity
 PRODUCT_SYSTEM_SERVER_DEBUG_INFO := false
 
+# Do not include other java mini debug info
+PRODUCT_OTHER_JAVA_DEBUG_INFO := false
+
 ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
 $(call enforce-product-packages-exist-internal,$(lastword $(_include_stack)),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
