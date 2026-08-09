LOSModified/android_vendor_lineage

config: Disable debug logging on non-eng builds
Debug and verbose logging is typically very chatty to the point of
slowing down a device. Since Lineage ships userdebug, debug logging
shouldn't be on by default on userdebug. Setting the persist log tag
properties can be done at runtime for debugging purposes when needed.

Change-Id: I2e5ec041bb0971b4be160fa889b140aec56dc96f

config/common.mk
@@ -37,6 +37,9 @@ endif
 
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
+
+# Disable debug and verbose logging by default
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += log.tag=I
 endif
 
 # Backup Tool

mayuresh-sources/lineage_qpr2_vendor_lineage

config/common.mk
@@ -45,6 +45,9 @@ endif
 
 # Disable extra StrictMode features on all non-engineering builds
 PRODUCT_PRODUCT_PROPERTIES += persist.sys.strictmode.disable=true
+
+# Disable debug and verbose logging by default
+PRODUCT_PRODUCT_PROPERTIES += log.tag=I
 endif
 
 # Backup Tool